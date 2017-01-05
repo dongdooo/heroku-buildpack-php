2595887ac4a4361dbd2798d3520c931ef2db2790
dongdooo <dongdooo@hotmail.com>
2017-01-05 13:31:19 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -100,7 +100,7 @@ for($i = 0; $i < count($messageText); $i++) {
 
 ////////// CURL ////////////
 
-$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+$ch = curl_init('https://graph.facebook.com/v2.6/211380252244527/messages?access_token='.$accessToken);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
